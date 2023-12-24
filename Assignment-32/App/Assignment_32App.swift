d984f37d343d99a4af9506919537310a79215717
ekakele <ekaakelenjeridze@gmail.com>
2023-12-24 19:32:36 +0400

    organize folder structure

---
+++ Assignment-32/App/Assignment_32App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_32App.swift
+//  Assignment-32
+//
+//  Created by Eka Kelenjeridze on 24.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_32App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
